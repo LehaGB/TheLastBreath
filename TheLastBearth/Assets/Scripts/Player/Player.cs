b399806b959e8b87916e9d98c4fba12289b06f2c
Алескей <manay.ru@mail.ru>
2026-09-21 19:16:10 +1000

    Реализовали анимацию в скрипте

--- TheLastBearth/Assets/Scripts/Player/Player.cs
+++ TheLastBearth/Assets/Scripts/Player/Player.cs
@@ -1,11 +1,15 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Rendering;
 
 public class Player : MonoBehaviour
 {
     [Tooltip("Скорость игрока")]
     [Header("Скорость игрока")]
     [SerializeField] private float moveSpeed = 3f;
+    [Space]
+    [Header("Аниматор игрока")]
+    [SerializeField] private Animator animator;
     private Rigidbody2D rb;
     private Vector2 movement;
     void Start()
@@ -21,6 +25,17 @@ public class Player : MonoBehaviour
 
     public void Move(InputAction.CallbackContext context)
     {
+        animator.SetBool("isMoving", true);
+
+        if (context.canceled)
+        {
+            animator.SetBool("isMoving", false);
+            animator.SetFloat("lastX", movement.x);
+            animator.SetFloat("lastY", movement.y);
+        }
         movement = context.ReadValue<Vector2>();
+
+        animator.SetFloat("InputX", movement.x);
+        animator.SetFloat("InputY", movement.y);
     }
 }
