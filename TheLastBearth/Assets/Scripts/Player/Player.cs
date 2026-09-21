6987c4793299fc4058d4d5982745c0ac00ea1ae5
Алескей <manay.ru@mail.ru>
2026-09-21 11:06:44 +1000

    Create Player Action

--- TheLastBearth/Assets/Scripts/Player/Player.cs
+++ TheLastBearth/Assets/Scripts/Player/Player.cs
@@ -2,15 +2,17 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [Tooltip("Скорость игрока")]
+    [Header("Скорость игрока")]
+    [SerializeField] private float moveSpeed = 3f;
+    private Rigidbody2D rb;
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        rb.linearVelocity = new Vector2(1f, 0f) * moveSpeed;
     }
 }
